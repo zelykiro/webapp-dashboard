87f4fad11ae3d92d89ee80fd54f5807ffade46b4
zelykiro <zelykiro@protonmail.com>
2021-10-19 14:18:34 +0530

    Fix minor issues

--- js/app.js
+++ js/app.js
@@ -21,10 +21,9 @@ function loadSavedSettings() {
 	if (savedSettings.length) {
 		emailNotif.checked = savedSettings[0]["emailNotification"];
 		profileVis.checked = savedSettings[0]["profileVisibility"];
-		if (savedSettings[0]["timezone"])
-			document
-				.querySelector(`[value="${savedSettings[0]["timezone"]}"]`)
-				.setAttribute("selected", true);
+		document
+			.querySelector(`[value="${savedSettings[0]["timezone"]}"]`)
+			.setAttribute("selected", true);
 	}
 	checkBoxLabel();
 }
